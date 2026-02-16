82be1e8be4e25827b5eb0da7817b235efc7ad247
hm0984-lab <hm0984@srmist.edu.in>
2026-02-16 20:25:56 +0530

    Completed UC2: Added 7-line ASCII banner

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,13 +1,20 @@
 /**
  * OOPS Banner App - Use Case 1
  * This program prints the literal text "OOPS" to the console.
- *
+ /**
+ * OOPS Banner App - Use Case 2
  * @author Harikrishanan
- * @version 1.0
+ * @version 2.0
  */
 public class OOPSBannerApp {
     public static void main(String[] args) {
-        // Printing the literal text "OOPS" as required by UC1
-        System.out.println("OOPS");
+        // Each line represents a horizontal slice of the word "OOPS"
+        System.out.println(" **** **** **** **** ");
+        System.out.println("* * * * * * * ");
+        System.out.println("* * * * * * * ");
+        System.out.println("* * * * **** **** ");
+        System.out.println("* * * * * *");
+        System.out.println("* * * * * *");
+        System.out.println(" **** **** * **** ");
     }
 }
