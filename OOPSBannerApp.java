1050a0554b022f030e7f7fa53b0a1fc22b1484bd
hm0984-lab <hm0984@srmist.edu.in>
2026-02-16 09:03:45 +0530

    Completed UC1: Printing OOPS to console

---
+++ OOPSBannerApp.java
@@ -0,0 +1,13 @@
+/**
+ * OOPS Banner App - Use Case 1
+ * This program prints the literal text "OOPS" to the console.
+ *
+ * @author Harikrishanan
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+        // Printing the literal text "OOPS" as required by UC1
+        System.out.println("OOPS");
+    }
+}
