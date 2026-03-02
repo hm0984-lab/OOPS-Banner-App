1cc90e50092e0aaa16159f2b9384b874e8353a55
hm0984-lab <hm0984@srmist.edu.in>
2026-03-02 08:37:03 +0530

    UC5 - Inline Array Initialization

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,20 +1,19 @@
-/**
- * OOPS Banner App - Use Case 1
- * This program prints the literal text "OOPS" to the console.
- /**
- * OOPS Banner App - Use Case 2
- * @author Harikrishanan
- * @version 2.0
- */
 public class OOPSBannerApp {
+
     public static void main(String[] args) {
-        // Each line represents a horizontal slice of the word "OOPS"
-        System.out.println(" **** **** **** **** ");
-        System.out.println("* * * * * * * ");
-        System.out.println("* * * * * * * ");
-        System.out.println("* * * * **** **** ");
-        System.out.println("* * * * * *");
-        System.out.println("* * * * * *");
-        System.out.println(" **** **** * **** ");
+
+        String[] lines = {
+            String.join("   ", " *** ", " *** ", " **** ", " ***** "),
+            String.join("   ", "*   *", "*   *", "*   * ", "*     "),
+            String.join("   ", "*   *", "*   *", "*   * ", "*     "),
+            String.join("   ", "*   *", "*   *", "****  ", " **** "),
+            String.join("   ", "*   *", "*   *", "*     ", "     *"),
+            String.join("   ", "*   *", "*   *", "*     ", "     *"),
+            String.join("   ", " *** ", " *** ", "*     ", " **** ")
+        };
+
+        for (String line : lines) {
+            System.out.println(line);
+        }
     }
 }
